41c3661e9f7618b18f47c47057654f7e4a3747de
Lucas Silvestre <lukas.silvestre@gmail.com>
2013-12-06 22:19:40 +0000

    Implements the client

---
+++ ServicesClient.java
@@ -0,0 +1,83 @@
+// Required imports
+import Kamarad.*;
+import org.omg.CORBA.*;
+import java.io.*;
+
+public class ServicesClient {
+	public static void main(String args[]) {
+		try {
+
+			//Initialize the ORB
+			ORB orb = ORB.init(args, null);
+			//Read the object Reference for the ControlsAccountServant
+			BufferedReader br = new BufferedReader(new FileReader("KamaradIOR"));
+			String ior = br.readLine();
+			//Convert the string object reference to an object
+			org.omg.CORBA.Object obj = orb.string_to_object(ior);
+			//Convert the object to the correct type i.e. ControlsAccount
+			ControlsAccount servicesRef = ControlsAccountHelper.narrow(obj);
+
+
+			Console console = System.console(); // Initialize console to get user option
+			int mobileNumber = 0; // Initialize var
+			String password = ""; // Initialize var
+			int input = 0; // Initialize var
+			String inputStr; // Initialize var
+
+			if(args[0] == null) {
+				System.out.println("Param missing");
+			}
+			mobileNumber = Integer.parseInt(args[0]);
+			float amount;
+
+			// inputStr = console.readLine("Password:");
+			// password = inputStr.trim();
+
+			Kamarad.Account account = servicesRef.returnAccount(mobileNumber);
+
+			//Shows options to user
+			System.out.println("Choose one of the following options:");
+			System.out.println("1 for Top Up Credit");
+			System.out.println("2 for Transfer Credit to Other Mobile Numbers");
+			System.out.println("3 for Buy Extra Options");
+			System.out.println("4 for View Credit Balance");
+			System.out.println("9 for Exit");
+
+			// Iterate while user do not select exit option (9)
+			while	(input != 9) {
+				// Ask user for option
+				inputStr = console.readLine("Enter input:");
+				input = Integer.parseInt(inputStr.trim());
+
+				switch (input){
+	        case 1: // Option for top up
+	        	amount = Float.parseFloat(console.readLine("Amount:"));
+	        	account = servicesRef.topUp(account.getNumber(), amount);
+	        	if(account == null) System.out.println("action failled!");
+	        	break;
+	        case 2: // Option for transfer credit
+	        	amount = Float.parseFloat(console.readLine("Amount:"));
+	        	mobileNumber = Integer.parseInt(console.readLine("Destination number:"));
+	        	account = servicesRef.transfer(account.getNumber(), amount, mobileNumber);
+	        	break;
+	        case 3:  // Option for buy extra options
+	        	break;
+	        case 4:  // Option for view credit
+	        	System.out.println("Total Credit: " + account.getBalance());
+	        	break;
+	        case 9:  // Option to exit
+	        	break;
+	        default: 
+	        	System.out.println("your input is invalid!");
+	        	break;
+	    	}
+			}
+		// exit message
+		System.out.println("bye!");
+		}
+		catch(Exception e) {
+			System.out.println("ERROR : " + e);
+			e.printStackTrace(System.out);
+		}
+	}
+}
